924a4b1296939fbc2c12c61d9e576f7ac8e167b5
wanglixin <wanglixin@icourt.cc>
2019-01-01 19:27:07 +0800

    添加单链表练习

--- src/List/linkedlist/RLinkedList.java
+++ src/List/linkedlist/RLinkedList.java
@@ -278,4 +278,341 @@ public class RLinkedList<T> implements RList<T> {
             return result;
         }
     }
+
+    /******************************************练习测试*****************************************/
+    /**
+     * 把该链表逆置
+     * 例如链表为 3->7->10 , 逆置后变为  10->7->3
+     */
+    public void reverse() {
+        if (isEmpty() || size < 2) {
+            return;
+        }
+        // 麻烦方法
+//        Node<T> pre;
+//        for (int i = size - 2; i >= 0; i--) {
+//            pre = getNodeByIndex(i);
+//            pre.next.next = pre;
+//            pre.next = null;
+//        }
+//        Node<T> temp = first;
+//        first = last;
+//        last = temp;
+
+        // 简单做法
+        last = first;
+        Node<T> pre = first;
+        Node<T> node = first.next;
+        Node<T> nextNode = first.next.next;
+        while (nextNode != null) {
+            node.next = pre;
+            pre = node;
+            node = nextNode;
+            nextNode = nextNode.next;
+        }
+        node.next = pre;
+        first = node;
+    }
+
+    /**
+     * 删除一个单链表的前半部分
+     * 例如：list = 2->5->7->8 , 删除以后的值为 7->8
+     * 如果list = 2->5->7->8->10 ,删除以后的值为7,8,10
+     */
+    public void removeFirstHalf() {
+        if (isEmpty() || size < 2) {
+            return;
+        }
+        Node<T> f = first;
+        Node<T> node = getNodeByIndex(size / 2 - 1);
+        first = node.next;
+        node.next = null;
+        size = size / 2 + size % 2;
+        // 清理前半部分
+        while (f != null) {
+            Node<T> temp = f;
+            f = f.next;
+            temp.next = null;
+            temp.item = null;
+        }
+    }
+
+    /**
+     * 从第i个元素开始， 删除length 个元素 ， 注意i从0开始
+     */
+    public void remove(int i, int length) {
+        if (isEmpty() || length < 1 || length > size) {
+            return;
+        }
+        length = Math.min(length, size - i);
+        Node<T> endNode = getNodeByIndex(i + length - 1);
+        // 尾节点
+        if (endNode.next == null) {
+            last = null;
+        }
+        Node<T> node;
+        if (i == 0) {
+            node = first;
+            first = endNode.next;
+        } else {
+            Node<T> pre = getNodeByIndex(i - 1);
+            node = pre.next;
+            pre.next = endNode.next;
+        }
+        endNode.next = null;
+        size -= length;
+        // 销毁各个节点
+        while (node != null) {
+            Node<T> temp = node;
+            node = node.next;
+            temp.next = null;
+            temp.item = null;
+        }
+    }
+
+    /**
+     * 假定当前链表和list均包含已升序排列的整数
+     * 从当前链表中取出那些list所指定的元素
+     * 例如当前链表 = 11->101->201->301->401->501->601->701
+     * listB = 1->3->4->6
+     * 返回的结果应该是[101,301,401,601]
+     */
+    @SuppressWarnings("unchecked")
+    public T[] getElements(RLinkedList<Integer> list) {
+
+        if (list == null || list.isEmpty()) {
+            return null;
+        }
+        int count = Math.min(size, list.size());
+        Object[] results = new Object[count];
+        int i = 0;
+        for (Node<Integer> node = list.first; node != null; node = node.next) {
+            if (node.item >= size) {
+                break;
+            }
+            results[i] = getNodeByIndex(node.item).item;
+            i++;
+        }
+        if (i < count) {
+            Object[] subResult = new Object[i];
+            System.arraycopy(results, 0, subResult, 0, i);
+            return (T[]) subResult;
+        }
+        return (T[]) results;
+    }
+
+    /**
+     * 已知链表中的元素以值递增有序排列，并以单链表作存储结构。
+     * 从当前链表中中删除在list中出现的元素
+     */
+    public void subtract(RLinkedList<Integer> list) {
+        if (list == null || list.isEmpty()) {
+            return;
+        }
+
+        if (list.first.item > (Integer) this.last.item
+                || list.last.item < (Integer) this.first.item) {
+            return;
+        }
+        int index = 0;
+        Node<T> node = first;
+        while (node != null) {
+            if (index == list.size) {
+                break;
+            }
+            Node<T> temp = node;
+            node = node.next;
+            for (Node<Integer> listNode = list.getNodeByIndex(index);
+                 listNode != null; listNode = listNode.next) {
+                if (listNode.item == temp.item) {
+                    remove(temp.item);
+                    break;
+                } else if (listNode.item < (Integer) temp.item) {
+                    index++;
+                }
+            }
+        }
+    }
+
+    /**
+     * 已知当前链表中的元素以值递增有序排列，并以单链表作存储结构。
+     * 删除表中所有值相同的多余元素（使得操作后的线性表中所有元素的值均不相同）
+     */
+    public void removeDuplicateValues() {
+        if (isEmpty() || size < 2) {
+            return;
+        }
+        T preVal = first.item;
+        for (Node<T> node = first.next; node != null; node = node.next) {
+            if (node.item.equals(preVal)) {
+                remove(preVal);
+            } else {
+                preVal = node.item;
+            }
+        }
+    }
+
+    /**
+     * 已知链表中的元素以值递增有序排列，并以单链表作存储结构。
+     * 试写一高效的算法，删除表中所有值大于min且小于max的元素（若表中存在这样的元素）
+     */
+    public void removeRange(int min, int max) {
+
+        if (max < min || isEmpty() || min > (Integer) last.item || max < (Integer) first.item) {
+            return;
+        }
+        int minVal = Math.max(min, (Integer) first.item);
+        int maxVal = Math.min(max, (Integer) last.item);
+        Node<T> node = first;
+        while (node != null) {
+            Node<T> temp = node;
+            node = node.next;
+            if ((Integer) temp.item > maxVal) {
+                break;
+            } else if ((Integer) temp.item >= minVal && (Integer) temp.item <= maxVal) {
+                remove(temp.item);
+            }
+        }
+    }
+
+    /**
+     * 123456789876543
+     * 假设当前链表和参数list指定的链表均以元素依值递增有序排列（同一表中的元素值各不相同）
+     * 现要求生成新链表C，其元素为当前链表和list中元素的交集，且表C中的元素有依值递增有序排列
+     */
+    @SuppressWarnings("unchecked")
+    public RLinkedList<Integer> intersection(RLinkedList<Integer> list) {
+        if (isEmpty()) {
+            return list;
+        }
+        if (list == null || list.isEmpty()) {
+            return (RLinkedList<Integer>) this;
+        }
+        RLinkedList<Integer> resultList = new RLinkedList<>();
+        Node<Integer> node;
+        if (list.first.item > (Integer) this.last.item) {
+            for (node = (Node<Integer>) first; node != null; node = node.next) {
+                resultList.add(node.item);
+            }
+            for (node = list.first; node != null; node = node.next) {
+                resultList.add(node.item);
+            }
+        } else if (list.last.item < (Integer) this.first.item) {
+            for (node = list.first; node != null; node = node.next) {
+                resultList.add(node.item);
+            }
+            for (node = (Node<Integer>) first; node != null; node = node.next) {
+                resultList.add(node.item);
+            }
+        } else {
+            Node<Integer> node1 = (Node<Integer>) first;
+            Node<Integer> node2 = list.first;
+            int preVal = Integer.MIN_VALUE;
+            for (int i = 0; i < size + list.size; i++) {
+                if (node1 == null && node2 == null) {
+                    break;
+                } else if (node1 == null) {
+                    resultList.add(node2.item);
+                    node2 = node2.next;
+                } else if (node2 == null) {
+                    resultList.add(node1.item);
+                    node1 = node1.next;
+                } else {
+                    int item1 = node1.item;
+                    int item2 = node2.item;
+                    if (item1 < item2) {
+                        if (item1 != preVal) {
+                            resultList.add(item1);
+                            preVal = item1;
+                        }
+                        node1 = node1.next;
+                    } else if (item1 > item2) {
+                        if (item2 != preVal) {
+                            resultList.add(item2);
+                            preVal = item2;
+                        }
+                        node2 = node2.next;
+                    } else {
+                        if (node1.item != preVal) {
+                            resultList.add(item1);
+                            preVal = item1;
+                        }
+                        node1 = node1.next;
+                        node2 = node2.next;
+                    }
+                }
+
+            }
+        }
+        return resultList;
+    }
+
+    /**
+     * 获取中间节点，假设不知道长度（快慢指针）
+     *
+     * @return
+     */
+    public T getMiddle() {
+        if (isEmpty()) {
+            return null;
+        }
+        Node<T> slowNode = first;
+        Node<T> quickNode = first;
+        while (quickNode != null && quickNode.next != null) {
+            slowNode = slowNode.next;
+            quickNode = quickNode.next.next;
+        }
+        return slowNode.item;
+    }
+
+    /**
+     * 如何判断一个字符串是否是回文字符串的问题
+     */
+    public boolean isPalindrome() {
+        if (isEmpty()) {
+            return false;
+        } else if (size == 1) {
+            return true;
+        } else if (size == 2) {
+            if (first.item.equals(first.next.item)) {
+                return true;
+            }
+            return false;
+        }
+        Node<T> slowNode = first;
+        Node<T> quickNode = first;
+        Node<T> pre = null;
+        Node<T> nextNode = first.next;
+        while (quickNode != null && quickNode.next != null) {
+            quickNode = quickNode.next.next;
+            slowNode.next = pre;
+            pre = slowNode;
+            slowNode = nextNode;
+            nextNode = nextNode.next;
+        }
+        slowNode.next = nextNode;
+        Node<T> temp = pre;
+        // 偶数
+        if (quickNode == null) {
+            if (!pre.item.equals(slowNode.item)) {
+                return false;
+            }
+            pre = pre.next;
+            temp.next = slowNode;
+            slowNode = temp;
+            temp = pre;
+        }
+        boolean result = true;
+        while (pre != null) {
+            if (!pre.item.equals(nextNode.item)) {
+                result = false;
+            }
+            nextNode = nextNode.next;
+            pre = pre.next;
+            temp.next = slowNode;
+            slowNode = temp;
+            temp = pre;
+        }
+        return result;
+    }
 }
--- src/List/linkedlist/RLinkedListTest.java
+++ src/List/linkedlist/RLinkedListTest.java
@@ -229,4 +229,332 @@ public class RLinkedListTest {
         }
         Assert.assertEquals(5, linkedList.size());
     }
+
+    /******************************************练习测试*****************************************/
+    /**
+     * 把该链表逆置
+     * 例如链表为 3->7->10 , 逆置后变为  10->7->3
+     */
+    @Test
+    public void reverse() {
+        RLinkedList<Integer> linkedList = new RLinkedList<>();
+        linkedList.add(3);
+        linkedList.add(7);
+        linkedList.add(10);
+        linkedList.add(12);
+        Assert.assertEquals(3, linkedList.get(0).intValue());
+        Assert.assertEquals(7, linkedList.get(1).intValue());
+        Assert.assertEquals(10, linkedList.get(2).intValue());
+        Assert.assertEquals(12, linkedList.get(3).intValue());
+
+        linkedList.reverse();
+        Assert.assertEquals(12, linkedList.get(0).intValue());
+        Assert.assertEquals(10, linkedList.get(1).intValue());
+        Assert.assertEquals(7, linkedList.get(2).intValue());
+        Assert.assertEquals(3, linkedList.get(3).intValue());
+    }
+
+    /**
+     * 删除一个单链表的前半部分
+     * 例如：list = 2->5->7->8 , 删除以后的值为 7->8
+     * 如果list = 2->5->7->8->10 ,删除以后的值为7,8,10
+     */
+    @Test
+    public void removeFirstHalf() {
+        RLinkedList<Integer> linkedList = new RLinkedList<>();
+        linkedList.add(2);
+        linkedList.add(5);
+        linkedList.add(7);
+        linkedList.add(8);
+        linkedList.add(9);
+
+        linkedList.removeFirstHalf();
+        Assert.assertEquals(3, linkedList.size());
+
+        Assert.assertEquals(7, linkedList.get(0).intValue());
+        Assert.assertEquals(8, linkedList.get(1).intValue());
+        Assert.assertEquals(9, linkedList.get(2).intValue());
+    }
+
+    /**
+     * 从第i个元素开始， 删除length 个元素 ， 注意i从0开始
+     */
+    @Test
+    public void remove() {
+        RLinkedList<Integer> linkedList = new RLinkedList<>();
+        linkedList.add(2);
+        linkedList.add(5);
+        linkedList.add(7);
+        linkedList.add(8);
+        linkedList.add(9);
+
+        linkedList.remove(0, 4);
+        Assert.assertFalse(linkedList.isEmpty());
+        Assert.assertEquals(1, linkedList.size());
+        Assert.assertEquals(9, linkedList.get(0).intValue());
+
+        RLinkedList<Integer> linkedList1 = new RLinkedList<>();
+        linkedList1.add(2);
+        linkedList1.add(5);
+        linkedList1.add(7);
+        linkedList1.add(8);
+        linkedList1.add(9);
+        linkedList1.add(12);
+        linkedList1.add(18);
+
+        linkedList1.remove(2, 4);
+        Assert.assertFalse(linkedList1.isEmpty());
+        Assert.assertEquals(3, linkedList1.size());
+        Assert.assertEquals(2, linkedList1.get(0).intValue());
+        Assert.assertEquals(5, linkedList1.get(1).intValue());
+        Assert.assertEquals(18, linkedList1.get(2).intValue());
+    }
+
+    /**
+     * 假定当前链表和list均包含已升序排列的整数
+     * 从当前链表中取出那些list所指定的元素
+     * 例如当前链表 = 11->101->201->301->401->501->601->701
+     * listB = 1->3->4->6
+     * 返回的结果应该是[101,301,401,601]
+     */
+    @Test
+    public void getElementsTest() {
+        RLinkedList<Integer> linkedList1 = new RLinkedList<>();
+        linkedList1.add(11);
+        linkedList1.add(101);
+        linkedList1.add(201);
+        linkedList1.add(301);
+        linkedList1.add(401);
+        linkedList1.add(501);
+        linkedList1.add(601);
+        linkedList1.add(701);
+
+        RLinkedList<Integer> linkedList2 = new RLinkedList<>();
+        linkedList2.add(1);
+        linkedList2.add(3);
+        linkedList2.add(4);
+        linkedList2.add(6);
+
+        Object[] elements = linkedList1.getElements(linkedList2);
+        Assert.assertEquals(4, elements.length);
+        Assert.assertEquals(101, elements[0]);
+        Assert.assertEquals(301, elements[1]);
+        Assert.assertEquals(401, elements[2]);
+        Assert.assertEquals(601, elements[3]);
+    }
+
+    /**
+     * 已知链表中的元素以值递增有序排列，并以单链表作存储结构。
+     * 从当前链表中中删除在list中出现的元素
+     */
+    @Test
+    public void subtract() {
+        RLinkedList<Integer> linkedList1 = new RLinkedList<>();
+        linkedList1.add(1);
+        linkedList1.add(2);
+        linkedList1.add(4);
+        linkedList1.add(5);
+        linkedList1.add(7);
+        linkedList1.add(8);
+        linkedList1.add(9);
+        linkedList1.add(10);
+        Assert.assertEquals(8, linkedList1.size());
+
+        RLinkedList<Integer> linkedList2 = new RLinkedList<>();
+        linkedList2.add(1);
+        linkedList2.add(4);
+        linkedList2.add(7);
+        linkedList2.add(8);
+        linkedList2.add(20);
+
+        linkedList1.subtract(linkedList2);
+        Assert.assertEquals(4, linkedList1.size());
+    }
+
+    /**
+     * 已知当前链表中的元素以值递增有序排列，并以单链表作存储结构。
+     * 删除表中所有值相同的多余元素（使得操作后的线性表中所有元素的值均不相同）
+     */
+    @Test
+    public void removeDuplicateValues() {
+
+        RLinkedList<Integer> linkedList1 = new RLinkedList<>();
+        linkedList1.add(0);
+        linkedList1.add(0);
+        linkedList1.add(1);
+        linkedList1.add(2);
+        linkedList1.add(2);
+        linkedList1.add(3);
+        linkedList1.add(4);
+        linkedList1.add(4);
+        linkedList1.add(4);
+        linkedList1.add(5);
+        linkedList1.add(6);
+        linkedList1.add(7);
+        linkedList1.add(8);
+        linkedList1.add(9);
+        linkedList1.add(9);
+
+        Assert.assertEquals(15, linkedList1.size());
+
+        linkedList1.removeDuplicateValues();
+        Assert.assertEquals(10, linkedList1.size());
+        for (int i = 0; i < 10; i++) {
+            Assert.assertEquals(i, linkedList1.get(i).intValue());
+        }
+    }
+
+    /**
+     * 已知链表中的元素以值递增有序排列，并以单链表作存储结构。
+     * 试写一高效的算法，删除表中所有值大于min且小于max的元素（若表中存在这样的元素）
+     */
+    @Test
+    public void removeRange() {
+        RLinkedList<Integer> linkedList1 = new RLinkedList<>();
+        linkedList1.add(0);
+        linkedList1.add(0);
+        linkedList1.add(1);
+        linkedList1.add(2);
+        linkedList1.add(2);
+        linkedList1.add(3);
+        linkedList1.add(4);
+        linkedList1.add(4);
+        linkedList1.add(4);
+        linkedList1.add(5);
+        linkedList1.add(6);
+        linkedList1.add(7);
+        linkedList1.add(8);
+        linkedList1.add(9);
+        linkedList1.add(9);
+
+//        linkedList1.removeRange(1,6);
+//        Assert.assertEquals(6,linkedList1.size());
+//        Assert.assertEquals(0, linkedList1.get(0).intValue());
+//        Assert.assertEquals(0, linkedList1.get(1).intValue());
+//        Assert.assertEquals(7, linkedList1.get(2).intValue());
+//        Assert.assertEquals(8, linkedList1.get(3).intValue());
+//        Assert.assertEquals(9, linkedList1.get(4).intValue());
+//        Assert.assertEquals(9, linkedList1.get(5).intValue());
+
+//        linkedList1.removeRange(1, 10);
+//        Assert.assertEquals(2, linkedList1.size());
+//        Assert.assertEquals(0, linkedList1.get(0).intValue());
+//        Assert.assertEquals(0, linkedList1.get(1).intValue());
+
+//        linkedList1.removeRange(0, 4);
+//        Assert.assertEquals(6, linkedList1.size());
+//        Assert.assertEquals(5, linkedList1.get(0).intValue());
+//        Assert.assertEquals(6, linkedList1.get(1).intValue());
+//        Assert.assertEquals(7, linkedList1.get(2).intValue());
+//        Assert.assertEquals(8, linkedList1.get(3).intValue());
+//        Assert.assertEquals(9, linkedList1.get(4).intValue());
+//        Assert.assertEquals(9, linkedList1.get(5).intValue());
+
+        linkedList1.removeRange(9, 9);
+        Assert.assertEquals(13, linkedList1.size());
+    }
+
+    /**
+     * 123456789876543
+     * 假设当前链表和参数list指定的链表均以元素依值递增有序排列（同一表中的元素值各不相同）
+     * 现要求生成新链表C，其元素为当前链表和list中元素的交集，且表C中的元素有依值递增有序排列
+     */
+    @Test
+    public void intersection() {
+        RLinkedList<Integer> linkedList1 = new RLinkedList<>();
+        linkedList1.add(1);
+        linkedList1.add(2);
+        linkedList1.add(4);
+        linkedList1.add(5);
+
+        RLinkedList<Integer> linkedList2 = new RLinkedList<>();
+        linkedList2.add(7);
+        linkedList2.add(8);
+        linkedList2.add(9);
+        linkedList2.add(10);
+
+        RLinkedList<Integer> integerRLinkedList1 = linkedList1.intersection(linkedList2);
+        Assert.assertEquals(8, integerRLinkedList1.size());
+        System.out.println(Arrays.toString(integerRLinkedList1.toArray()));
+
+        RLinkedList<Integer> integerRLinkedList2 = linkedList2.intersection(linkedList1);
+        Assert.assertEquals(8, integerRLinkedList2.size());
+        System.out.println(Arrays.toString(integerRLinkedList2.toArray()));
+
+        RLinkedList<Integer> linkedList3 = new RLinkedList<>();
+        linkedList3.add(1);
+        linkedList3.add(2);
+        linkedList3.add(4);
+        linkedList3.add(5);
+        linkedList3.add(7);
+        linkedList3.add(9);
+        linkedList3.add(16);
+        linkedList3.add(17);
+
+        RLinkedList<Integer> linkedList4 = new RLinkedList<>();
+        linkedList4.add(1);
+        linkedList4.add(3);
+        linkedList4.add(7);
+        linkedList4.add(8);
+        linkedList4.add(9);
+        linkedList4.add(10);
+        linkedList4.add(17);
+
+        RLinkedList<Integer> integerRLinkedList3 = linkedList3.intersection(linkedList4);
+        Assert.assertEquals(11, integerRLinkedList3.size());
+        System.out.println(Arrays.toString(integerRLinkedList3.toArray()));
+
+        RLinkedList<Integer> integerRLinkedList4 = linkedList4.intersection(linkedList3);
+        Assert.assertEquals(11, integerRLinkedList4.size());
+        System.out.println(Arrays.toString(integerRLinkedList4.toArray()));
+    }
+
+    /**
+     * 获取中间节点，假设不知道长度（快慢指针）
+     *
+     * @return
+     */
+    @Test
+    public void getMiddle() {
+
+        RLinkedList<Integer> linkedList1 = new RLinkedList<>();
+        linkedList1.add(1);
+        linkedList1.add(2);
+        linkedList1.add(4);
+        linkedList1.add(5);
+        Assert.assertEquals(4,linkedList1.getMiddle().intValue());
+
+        RLinkedList<Integer> linkedList2 = new RLinkedList<>();
+        linkedList2.add(6);
+        linkedList2.add(7);
+        linkedList2.add(8);
+        linkedList2.add(9);
+        linkedList2.add(10);
+        Assert.assertEquals(8,linkedList2.getMiddle().intValue());
+    }
+
+    /**
+     * 如何判断一个字符串是否是回文字符串的问题
+     */
+    @Test
+    public void isPalindrome() {
+        RLinkedList<Integer> linkedList1 = new RLinkedList<>();
+        linkedList1.add(1);
+        linkedList1.add(2);
+        linkedList1.add(2);
+        linkedList1.add(1);
+
+        Assert.assertTrue(linkedList1.isPalindrome());
+        System.out.println(Arrays.toString(linkedList1.toArray()));
+
+        RLinkedList<Integer> linkedList2 = new RLinkedList<>();
+        linkedList2.add(1);
+        linkedList2.add(2);
+        linkedList2.add(3);
+        linkedList2.add(2);
+        linkedList2.add(1);
+
+        Assert.assertTrue(linkedList2.isPalindrome());
+        System.out.println(Arrays.toString(linkedList2.toArray()));
+    }
 }
